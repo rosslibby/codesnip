b641720cd478b276c887b8027c65889546420eeb
Ross Libby <rosslibby92@gmail.com>
2025-09-18 18:49:08 -0400

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development",
+  entry: 'src/index.ts',
+  output: { path: __dirname + '/dist' },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        resolve: { extensions: ['.ts'] },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({ template: __dirname + '/src/index.html' }),
+  ],
+  devServer: {
+    static: { directory: __dirname + '/dist' },
+    compress: true,
+    port: 3000,
+  },
+};
+
